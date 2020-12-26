11143963b56dab44967d7a2d50e5ec0f554773cf
ppolsinelli <ppolsinelli@gmail.com>
2020-12-26 14:04:31 +0100

    Upgraded to Unity 2020.1. Created sample ppolsinelli Ink script.

--- InkAndUnity/Assets/Plugins/Ink/InkLibs/InkCompiler/Properties/AssemblyInfo.cs
+++ InkAndUnity/Assets/Plugins/Ink/InkLibs/InkCompiler/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo ("inklecate")]
 [assembly: InternalsVisibleTo ("ink-tests")]
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
